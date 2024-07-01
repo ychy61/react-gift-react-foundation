1a532317c058d05e50a540e42579213f7e0d528c
ychy61 <ychy010601@gmail.com>
2024-07-01 15:36:28 +0900

    refactor: Button.tsx 수정
    React.ButtonHTMLAttributes<HTMLButtonElement>를 사용하여 버튼의 모든
    속성을 포함하도록 할 수 있도록 수정
    styleMapper를 만들어 적용함

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,58 +1,74 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react';
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 
 // ButtonProps 인터페이스 정의
-export interface ButtonProps {
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  // HTML 버튼 요소의 속성을 상속
   theme: 'primary' | 'secondary'; // 버튼의 테마 (primary 또는 secondary)
   size: 'small' | 'medium' | 'large' | 'responsive'; // 버튼의 크기 (small, medium, large 또는 responsive)
   children: React.ReactNode; // 버튼 내부의 내용
-  onClick: () => void; // 버튼 클릭 시 호출될 함수
 }
+
+const sizeMapper = (size: ButtonProps['size']) => {
+  switch (size) {
+    case 'small':
+      return css`
+        font-size: 0.75rem;
+        padding: 0.25rem 0.5rem;
+      `;
+    case 'large':
+      return css`
+        font-size: 1.25rem;
+        padding: 0.75rem 1.5rem;
+      `;
+    case 'responsive':
+      return css`
+        font-size: 1rem;
+        padding: 0.5rem 1rem;
+
+        @media (max-width: 600px) {
+          font-size: 0.75rem;
+          padding: 0.25rem 0.5rem;
+        }
+      `;
+    default:
+      return css`
+        font-size: 1rem;
+        padding: 0.5rem 1rem;
+      `;
+  }
+};
+
+const themeMapper = (theme: ButtonProps['theme']) => {
+  switch (theme) {
+    case 'primary':
+      return css`
+        background-color: #007bff;
+        color: white;
+      `;
+    case 'secondary':
+      return css`
+        background-color: #6c757d;
+        color: white;
+      `;
+    default:
+      return css`
+        background-color: #007bff;
+        color: white;
+      `;
+  }
+};
 
 // styled.button을 사용하여 버튼 스타일 정의
 const Button = styled.button<ButtonProps>`
   border: none;
   cursor: pointer;
-  font-size: ${(props) => {
-    switch (props.size) {
-      case 'small':
-        return '0.75rem'; // 작은 버튼의 글꼴 크기
-      case 'large':
-        return '1.25rem'; // 큰 버튼의 글꼴 크기
-      case 'responsive':
-        return '1rem'; // 반응형 버튼의 글꼴 크기
-      default:
-        return '1rem'; // 기본 글꼴 크기
-    }
-  }};
-  padding: ${(props) => {
-    switch (props.size) {
-      case 'small':
-        return '0.25rem 0.5rem'; // 작은 버튼의 패딩
-      case 'large':
-        return '0.75rem 1.5rem'; // 큰 버튼의 패딩
-      case 'responsive':
-        return '0.5rem 1rem'; // 반응형 버튼의 패딩
-      default:
-        return '0.5rem 1rem'; // 기본 패딩
-    }
-  }};
-  transition: background-color 0.3s ease; // 배경색 전환 효과
-  background-color: ${(props) =>
-    props.theme === 'primary' ? '#007bff' : '#6c757d'}; // 테마에 따른 배경색
-  color: white; // 글자색
-
-  @media (max-width: 600px) {
-    font-size: ${(props) =>
-      props.size === 'responsive'
-        ? '0.75rem'
-        : ''}; // 작은 화면에서 반응형 버튼의 글꼴 크기
-    padding: ${(props) =>
-      props.size === 'responsive'
-        ? '0.25rem 0.5rem'
-        : ''}; // 작은 화면에서 반응형 버튼의 패딩
-  }
+  transition: background-color 0.3s ease;
+  ${(props) => sizeMapper(props.size)}
+  ${(props) => themeMapper(props.theme)}
 `;
 
 // ButtonComponent 컴포넌트 정의
